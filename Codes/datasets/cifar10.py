a2a675de7532d017ae027b2c76890e215e93386a
Mazdak <mazdak.tey@gmail.com>
2025-07-16 16:18:54 +0330

    Reviewed CIFAR10

--- Codes/datasets/cifar10.py
+++ Codes/datasets/cifar10.py
@@ -3,31 +3,33 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
 import torchvision.transforms as transforms
 
-# CIFAR10 mean and std
-norm_mean = (0.4914, 0.4822, 0.4465)
-norm_std = (0.2471, 0.2435, 0.2616)
-
-# Transformations
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize(norm_mean, norm_std),
-])
-
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(norm_mean, norm_std),
-])
-
-
-def get_loaders(batch_size: int = 128, num_workers: int = 2, device: str = 'cuda'):
-    mu = torch.tensor(norm_mean).view(3,1,1).to(device)
-    std = torch.tensor(norm_std).view(3,1,1).to(device)
+def get_loaders(batch_size: int = 128, num_workers: int = 2, device: str = 'cuda', normalize_dataset: bool = True):
+    if normalize_dataset:
+        cifar10_mean = [0.4914, 0.4822, 0.4465] # equals np.mean(train_set.train_data, axis=(0,1,2))/255
+        cifar10_std = [0.2471, 0.2435, 0.2616] # equals np.std(train_set.train_data, axis=(0,1,2))/255
+    else:
+        cifar10_mean = [0., 0., 0.]
+        cifar10_std = [1., 1., 1.]
+    
+    mu = torch.tensor(cifar10_mean).view(3,1,1).to(device)
+    std = torch.tensor(cifar10_std).view(3,1,1).to(device)
+    
+        
+    train_transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(cifar10_mean, cifar10_std),
+        ])
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(cifar10_mean, cifar10_std),
+    ])
+    
 
     # Download the dataset
-    trainset = CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-    testset = CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    trainset = CIFAR10(root='./data', train=True, download=True, transform=train_transform)
+    testset = CIFAR10(root='./data', train=False, download=True, transform=test_transform)
 
     # Create the loaders
     trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
@@ -37,4 +39,8 @@ def get_loaders(batch_size: int = 128, num_workers: int = 2, device: str = 'cuda
     upper_limit = ((1 - mu)/ std).to(device)
     lower_limit = ((0 - mu)/ std).to(device)
 
-    return trainloader, testloader, upper_limit, lower_limit, std, 10
+    # Name of Classes
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    return trainloader, testloader, upper_limit, lower_limit, mu, std, classes, len(classes)
