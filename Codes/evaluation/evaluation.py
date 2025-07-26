92c20a800a98e1236683fb4d1385064aa7bdd70f
Mazdak <mazdak.tey@gmail.com>
2025-07-27 00:47:54 +0330

    Added initial evaluation code

--- Codes/evaluation/evaluation.py
+++ Codes/evaluation/evaluation.py
@@ -1,21 +1,20 @@
-import torch.nn.functional as F
+from ..utils import load_checkpoint
 from attacks.get_attack import get_attack
-from architectures.get_model import get_model
-from .alignment import calc_alignment
-from ..utils import save_checkpoint
+import torch.nn.functional as F
 
-def train(dataloader, model_name: str, num_classes: int, attack_name: str, attack_params, optimizer, scheduler, num_epochs: int=30, device:str="cuda"):
-    model = get_model(model_name, num_classes)
-    use_regularizer = attack_name in ["TRADES", "GradAlign", "ELLE"]
+def evaluate(dataloader, checkpoints_path: str, model_name: str, num_classes: int, attack_name: str, attack_params, num_epochs: int=30, device:str="cuda"):
+    reg_list_test = []
     
-    for epoch in range(num_epochs):
+    use_regularizer = attack_name in ["TRADES", "GradAlign", "ELLE"]
+    for epoch in range(num_epochs + 1):
+        model, _, _ = load_checkpoint(model_name, num_classes, f"{checkpoints_path}/model{str(epoch).zfill(3)}.pt")
+        model.eval()
+
         for i, data in enumerate(dataloader):
             if index_dataset:
                 images, labels, index = data[0].to(device), data[1].to(device), data[2]
             else:
                 images, labels = data[0].to(device), data[1].to(device)
-            # Zero out previous gradient accumulation
-            optimizer.zero_grad()
             # Determine attack
             attack = get_attack(attack_name)
             match attack_name:
@@ -31,28 +30,13 @@ def train(dataloader, model_name: str, num_classes: int, attack_name: str, attac
                     delta_fgsm_ep[index] = delta.clone().detach()
                 case _:
                     raise "Invalid Attack Method!"
-            
             # Add perturbation to original images
             adv_images = images + delta
-            if calculate_alignment:
-                adv_images.requires_grad = True
             # Forward pass with adversarial examples
             preds = model(adv_images)
             loss = F.cross_entropy(preds, labels)
-            # Add regularization term if needed
+            #Track Regularizer Value Per Batch
             if use_regularizer:
-                loss += reg_coeff * reg
-            # Backpropagate
-            loss.backward()
-            # Update weights
-            optimizer.step()
-            # Update Scheduler # TODO this is only for Cyclic LR not for MultiStep
-            scheduler.step()
-            if calculate_alignment:
-                alignment = calc_alignment(grad, adv_images)
-
-
-    # Save training checkpoint
-    save_checkpoint(model, optimizer, scheduler, f"{root_path}/checkpoints/model{str(epoch).zfill(3)}.pt")
-
-
+                reg = reg.cpu() if reg is not None else None
+                reg = reg.item()
+                reg_list_test.append(reg)
