60e385c615c7d027bfdabb77d0881c46455b78ec
Mazdak <mazdak.tey@gmail.com>
2025-07-27 11:27:19 +0330

    Modified train.py

--- Codes/main.py
+++ Codes/main.py
@@ -1,23 +1,30 @@
 from .training.train import train
 from .evaluation.evaluation import evaluate
+from .utils import create_directories
 import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--root_path", required=True)
-    parser.add_argument("--dataset", choices=["CIFAR10", "CIFAR100", "CINIC10", "SVHN", "TinyImageNet", "PathMNIST"], required=True)
-    parser.add_argument("--model", choices=["PreActResNet18", "ResNet18", "WideResNet28", "SENet18"], default="PreActResNet18", required=True)
+    parser.add_argument("--dataset", choices=["CIFAR10", "CIFAR100", "CINIC10", "SVHN", "TinyImageNet", "PathMNIST"], default="CIFAR10")
+    parser.add_argument("--model", choices=["PreActResNet18", "ResNet18", "WideResNet28", "SENet18"], default="PreActResNet18")
     parser.add_argument("--attack", choices=["SIA", "FGSM", "FGSM-RS", "NFGSM", "ZeroGrad", "ATAS", "TRADES", "PGD"], required=True)
     parser.add_argument("--epsilon", default=8 / 255)
     parser.add_argument("--epochs", type=int, default=30)
+    parser.add_argument("--initial_lr", default=0.01, help="May be overwritten by scheduler")
     parser.add_argument("--optimizer", choices=["SGD"], default="SGD")
+    parser.add_argument("--momentum", default=0.9)
+    parser.add_argument("--weight_decay", default=5e-4)
     parser.add_argument("--scheduler", choices=["Cyclic", "MultiStep", "CosineAnnealing"], default="Cyclic")
     parser.add_argument("--track_alignment", type=bool, default=True)
     return parser.parse_args()
 
 def main():
+    # Parse arguments
     args = parse_args()
-
-    train(args, optimizer, scheduler)
-    
-    
+    # Create nescessary directories
+    create_directories(args.root_path)
+    # Train model
+    train(args)
+    # Evaluate training
+    evaluate(args)
--- Codes/training/train.py
+++ Codes/training/train.py
@@ -1,58 +1,77 @@
 import torch.nn.functional as F
-from attacks.get_attack import get_attack
+from datasets.get_loaders import get_loaders
 from architectures.get_model import get_model
+from attacks.get_attack import get_attack
+from ..attacks.attack_params import attack_params_dict, regularizer_params_dict
 from .alignment import calc_alignment
-from ..utils import save_checkpoint
+from ..utils import save_checkpoint, get_device
+from .utils import get_optimizer, get_scheduler
 
-def train(dataloader, model_name: str, num_classes: int, attack_name: str, attack_params, optimizer, scheduler, num_epochs: int=30, device:str="cuda"):
-    model = get_model(model_name, num_classes)
-    use_regularizer = attack_name in ["TRADES", "GradAlign", "ELLE"]
-    
-    for epoch in range(num_epochs):
-        for i, data in enumerate(dataloader):
+def train(args):
+    # Get dataset loaders
+    trainloader, _, upper_limit, lower_limit, _, _, _, num_classes = get_loaders(args.dataset)
+    # Get model
+    model = get_model(args.model, num_classes)
+    # Set device
+    device = get_device()
+    # Get optimizer
+    optimizer = get_optimizer(args.optimizer, model)
+    # Get scheduler
+    scheduler = get_scheduler(args.scheduler, optimizer)
+    # Determine attack
+    attack = get_attack(args.attack)
+    # Get attack parameters
+    attack_params = attack_params_dict.get(args.attack, {}).copy()
+    # Get regularization coefficient if needed
+    use_regularizer = args.attack in ["TRADES", "GradAlign", "ELLE"]
+    index_dataset = args.attack in ["ATAS", "FGSM-EP"]
+    if use_regularizer:
+        reg_params = regularizer_params_dict.get(args.attack, {}).copy()
+
+
+    for epoch in range(args.epochs):
+        for i, data in enumerate(trainloader):
             if index_dataset:
                 images, labels, index = data[0].to(device), data[1].to(device), data[2]
             else:
                 images, labels = data[0].to(device), data[1].to(device)
             # Zero out previous gradient accumulation
             optimizer.zero_grad()
-            # Determine attack
-            attack = get_attack(attack_name)
-            match attack_name:
+            match args.attack:
                 case attack if attack in  ["FGSM", "FGSM-RS", "NFGSM", "ZeroGrad", "SIA", "PGD"]:
-                    delta, grad = attack(model, images, labels, **attack_params)
+                    delta, grad = attack(model, images, labels, upper_limit, lower_limit, **attack_params)
                 case attack if attack in ["TRADES", "GradAlign", "ELLE"]:
-                    delta, reg, grad = attack(model, images, labels, **attack_params)
+                    delta, reg, grad = attack(model, images, labels, upper_limit, lower_limit, **attack_params)
                 case "ATAS":
-                    delta, grad = attack(model, images, labels, index, **attack_params)
+                    delta, grad = attack(model, images, labels, index, upper_limit, lower_limit, **attack_params)
                     delta_atas[index] = delta.clone().detach()
                 case "FGSM-EP":
-                    delta, reg, grad = attack(model, images, labels, index, **attack_params)
+                    delta, reg, grad = attack(model, images, labels, index, upper_limit, lower_limit, **attack_params)
                     delta_fgsm_ep[index] = delta.clone().detach()
                 case _:
                     raise "Invalid Attack Method!"
             
             # Add perturbation to original images
             adv_images = images + delta
-            if calculate_alignment:
+            if args.track_alignment:
                 adv_images.requires_grad = True
             # Forward pass with adversarial examples
             preds = model(adv_images)
             loss = F.cross_entropy(preds, labels)
             # Add regularization term if needed
             if use_regularizer:
-                loss += reg_coeff * reg
+                loss += reg_params["reg"] * reg
             # Backpropagate
             loss.backward()
             # Update weights
             optimizer.step()
             # Update Scheduler # TODO this is only for Cyclic LR not for MultiStep
             scheduler.step()
-            if calculate_alignment:
+            if args.track_alignment:
                 alignment = calc_alignment(grad, adv_images)
-
-
+                if args.attack == "SIA":
+                    attack_params["alignment"] = alignment # Save as attack param to use in the next batch for SIA
     # Save training checkpoint
-    save_checkpoint(model, optimizer, scheduler, f"{root_path}/checkpoints/model{str(epoch).zfill(3)}.pt")
+    save_checkpoint(model, optimizer, scheduler, f"{args.root_path}/checkpoints/model{str(epoch).zfill(3)}.pt")
 
 
