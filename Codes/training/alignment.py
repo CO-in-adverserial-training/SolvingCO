8a673021a43fe6fe956291de585946572c9f9532
Mazdak Teymourian <mazdak.tey@gmail.com>
2025-07-28 21:35:05 +0000

    Fixed bugs

--- Codes/training/alignment.py
+++ Codes/training/alignment.py
@@ -1,5 +1,5 @@
 import torch.nn.functional as F
 
-def calc_alignment(input_grad, adv_images):
-    backprop_grad = adv_images.grad.clone().detach()
+def calc_alignment(input_grad, delta):
+    backprop_grad = delta.grad.clone().detach()
     return F.cosine_similarity(backprop_grad.view(backprop_grad.shape[0], -1), input_grad.view(input_grad.shape[0], -1)).mean().item()
