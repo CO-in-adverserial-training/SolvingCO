4b3000211e3afdccdcf875f5dc4b87a904247084
Farzan <74285751+FarzanRahmani@users.noreply.github.com>
2025-08-02 23:14:25 +0330

    updated to incorporate vit

--- Codes/main.py
+++ Codes/main.py
@@ -7,10 +7,11 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--root_path", required=True)
     parser.add_argument("--dataset", choices=["CIFAR10", "CIFAR100", "CINIC10", "SVHN", "TinyImageNet", "PathMNIST"], default="CIFAR10")
+    parser.add_argument("--dataset_image_size", type=int, default=32)
     parser.add_argument("--batch_size", type=int, default=128)
     parser.add_argument("--num_workers", type=int, default=2)
     parser.add_argument("--normalize_dataset", action="store_true")
-    parser.add_argument("--model", choices=["PreActResNet18", "ResNet18", "WideResNet28", "SENet18"], default="PreActResNet18")
+    parser.add_argument("--model", choices=["PreActResNet18", "ResNet18", "WideResNet28", "SENet18", "VitSmall", "VitBase"], default="PreActResNet18")
     parser.add_argument("--attack", choices=["SIA", "FGSM", "FGSM-RS", "NFGSM", "ZeroGrad", "ATAS", "TRADES", "PGD"], required=True)
     parser.add_argument("--epsilon", type=float, default=8 / 255)
     parser.add_argument("--epochs", type=int, default=30)
@@ -33,7 +34,7 @@ def main():
     # Set seed
     set_seeds()
     # Train model
-    train(args, device)
+    train(args, device, args.dataset_image_size)
     # Evaluate training
     evaluate(args, device)
 
