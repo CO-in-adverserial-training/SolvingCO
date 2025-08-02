e87f420f6b840529d751d4dbf3d4eb0dee9029b8
Farzan <74285751+FarzanRahmani@users.noreply.github.com>
2025-08-02 23:13:38 +0330

    add vit to get model

--- Codes/architectures/get_model.py
+++ Codes/architectures/get_model.py
@@ -2,8 +2,10 @@ from .resnet import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
 from .preact_resnet import PreActResNet18, PreActResNet34, PreActResNet50, PreActResNet101, PreActResNet152
 from .wide_resnet import WideResNet28, WideResNet34
 from .senet import SENet18
+from .vit_small import VitSmallPatch16_224
+from .vit_base import VitBasePatch16_224
 
-def get_model(model_name: str, num_classes: int=10):
+def get_model(model_name: str, num_classes: int=10, img_size: int=32):
     match model_name:
         case "ResNet18":
             return ResNet18(num_classes=num_classes)
@@ -31,5 +33,9 @@ def get_model(model_name: str, num_classes: int=10):
             return WideResNet34(num_classes=num_classes)
         case "SENet18":
             return SENet18(num_classes=num_classes)
+        case "VitSmall":
+            return VitSmallPatch16_224(num_classes=num_classes, img_size=img_size)
+        case "VitBase":
+            return VitBasePatch16_224(num_classes=num_classes, img_size=img_size)
         case _:
             raise ValueError("Invalid Model!")
