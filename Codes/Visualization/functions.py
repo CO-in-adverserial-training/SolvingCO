9ebf5993ce96b2508dff6f46af18914d9d299aa9
Mazdak Teymourian <mazdak.tey@gmail.com>
2025-08-03 19:03:48 +0330

    Added loss, accuracy and alignment plot functions

--- Codes/Visualization/functions.py
+++ Codes/Visualization/functions.py
@@ -1,4 +1,6 @@
 import numpy as np
+import json
+import matplotlib.pyplot as plt
 
 # A Function For Automatically Detecting The Time Range Of CO Occurance
 def detect_window_range(window_size, alignments, tail_portion=0.25):
@@ -16,9 +18,38 @@ def detect_window_range(window_size, alignments, tail_portion=0.25):
             capture_next = True
     return window_list
 
-def plot_loss_and_accuracy(metrics_path: str):
-    pass
+def plot_loss_and_accuracy(args, training_loss, training_accuracy, attack_evaluation_loss, attack_evaluation_accuracy,
+                            fgsm_evaluation_loss, fgsm_evaluation_accuracy, pgd_evaluation_loss, pgd_evaluation_accuracy):
+    figure, axis = plt.subplots(1,2, figsize=(15,5))
+    axis[0].plot(np.arange(len(training_loss)) ,training_loss, '-o', label=f'Train Loss {args.attack}')
+    axis[0].plot(np.arange(len(attack_evaluation_loss)) ,attack_evaluation_loss, '-o', label=f'Test Loss {args.attack}')
+    axis[0].plot(np.arange(len(fgsm_evaluation_loss)) ,fgsm_evaluation_loss, '-o', label='Test Loss FGSM')
+    axis[0].plot(np.arange(len(pgd_evaluation_loss)) ,pgd_evaluation_loss, '-o', label='Test Loss PGD')
+    axis[0].plot(np.arange(len(test_losses_benign)) ,test_losses_benign, '-o', label='Test Loss Benign')
+    axis[0].set_title("Loss vs. Epochs")
+    axis[0].set_xlabel("Epochs")
+    axis[0].set_ylabel("Loss")
+    axis[0].legend()
+    axis[0].grid()
+    axis[1].set_title("Accuracy vs. Epochs")
+    axis[1].set_xlabel("Epochs")
+    axis[1].set_ylabel("Accuracy")
+    axis[1].plot(np.arange(len(training_accuracy)),training_accuracy, '-o', label=f'Train Accuracy {args.attack}')
+    axis[1].plot(np.arange(len(attack_evaluation_accuracy)),attack_evaluation_accuracy, '-o', label=f'Test Accuracy {args.attack}')
+    axis[1].plot(np.arange(len(fgsm_evaluation_accuracy)),fgsm_evaluation_accuracy, '-o', label='Test Accuracy FGSM')
+    axis[1].plot(np.arange(len(pgd_evaluation_accuracy)),pgd_evaluation_accuracy, '-o', label='Test Accuracy PGD')
+    axis[1].plot(np.arange(len(accs_benign)),accs_benign, '-o', label='Test Accuracy Benign')
+    axis[1].legend()
+    axis[1].grid(visible=True, which= 'minor', color='k', linestyle='-', alpha=0.4)
+    axis[1].grid(visible=True, which= 'major', color='b', linestyle='-', alpha=0.8)
+    plt.minorticks_on()
+    plt.savefig(f"{args.root_path}/plots/loss_accuracy_plot.pdf")
+    plt.show()
 
 
-def plot_alignment(metrics_path: str):
-    pass
+def plot_alignment(args, alignments: list):
+    plt.figure(figsize=(15,6))
+    plt.plot(alignments)
+    plt.grid()
+    plt.savefig(f"{args.root_path}/plots/alignment_plot.pdf")
+    plt.show()
--- Codes/Visualization/visualize.py
+++ Codes/Visualization/visualize.py
@@ -1,12 +1,20 @@
+import json
 from Visualization.functions import plot_loss_and_accuracy, plot_alignment
 
 def visualize(args):
     training_metrics_path = f"{args.root_path}/raw_results/train_metrics_{args.attack}_{args.seed}.json"
     evaluation_metrics_path = f"{args.root_path}/raw_results/evaluation_metrics_{args.attack}_{args.seed}.json"
 
-    plot_loss_and_accuracy(training_metrics_path)
+    with open(training_metrics_path, "r") as f:
+        training_metrics = json.load(f)
 
-    plot_loss_and_accuracy(evaluation_metrics_path)
+    with open(evaluation_metrics_path, "r") as f:
+        evaluation_metrics = json.load(f)
+
+    plot_loss_and_accuracy(args, training_metrics["epoch_metrics"]["loss"], training_metrics["epoch_metrics"]["accuracy"],
+                            evaluation_metrics["attack_epoch_metrics"]["loss"], evaluation_metrics["attack_epoch_metrics"]["accuracy"],
+                            evaluation_metrics["fgsm_epoch_metrics"]["loss"], evaluation_metrics["fgsm_epoch_metrics"]["accuracy"],
+                            evaluation_metrics["pgd_epoch_metrics"]["loss"], evaluation_metrics["pgd_epoch_metrics"]["accuracy"])
 
     if args.track_alignment:
-        plot_alignment(training_metrics_path)
+        plot_alignment(args, training_metrics["alignment_values"])
