Fix dataset normalization

Codes/attacks/zerograd.py
@@ -2,7 +2,11 @@ import torch
 import torch.nn.functional as F
 
 
-def zero_grad(model, x, y, upper_limit, lower_limit, epsilon: float = 8/255, alpha: float = 2.0, q_val: float = 0.35, k: float = 1.0, clip: bool = True):
+def zero_grad(model, x, y, upper_limit, lower_limit, epsilon: float = 8/255, alpha: float = 10/255, q_val: float = 0.35, k: float = 1.0, clip: bool = True):
+    # Normalize perturbations
+    epsilon = (epsilon / std).view(1, -1, 1, 1)
+    alpha = (alpha / std).view(1, -1, 1, 1)
+    
     delta = torch.empty_like(x)
     if k != 0:
         for j in range(len(epsilon)):
@@ -16,8 +20,9 @@ def zero_grad(model, x, y, upper_limit, lower_limit, epsilon: float = 8/255, alp
     q_grad = torch.quantile(torch.abs(grad).view(grad.size(0), -1), q_val, dim=1)
     grad[torch.abs(grad) < q_grad.view(grad.size(0), 1, 1, 1)] = 0
 
+    delta = delta + alpha * torch.sign(grad)
     if clip:
-        delta = torch.clamp(delta + alpha * epsilon * torch.sign(grad), min=-epsilon, max=epsilon)
+        delta = torch.clamp(delta, min=-epsilon, max=epsilon)
     delta = torch.clamp(delta, lower_limit - x, upper_limit - x).detach()
 
     return delta, grad