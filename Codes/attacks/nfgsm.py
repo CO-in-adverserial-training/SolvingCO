f1d3a17fa21359c43211f4a0a598bb9879135385
Ramtin Moslemi <76493699+RamtinMoslemi@users.noreply.github.com>
2025-08-03 22:09:45 +0330

    Fix for dataset normalization

--- Codes/attacks/nfgsm.py
+++ Codes/attacks/nfgsm.py
@@ -2,7 +2,11 @@ import torch
 import torch.nn.functional as F
 
 
-def nfgsm(model, x, y, upper_limit, lower_limit, epsilon: float = 8/255, alpha: float = 8/255, k: float = 2.0):
+def nfgsm(model, x, y, upper_limit, lower_limit, mu, std, epsilon: float = 8/255, alpha: float = 8/255, k: float = 2.0):
+    # Normalize perturbations
+    epsilon = (epsilon / std).view(1, -1, 1, 1)
+    alpha = (alpha / std).view(1, -1, 1, 1)
+    
     # Initialize random step
     eta = torch.empty_like(x).uniform_(-k, k)
     eta *= epsilon.view(1, -1, 1, 1)  # Reshape epsilon for broadcasting
