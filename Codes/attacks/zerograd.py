40dd941e91c28f40ed590ca3c280ba9b094ba549
MazMazTey <mazdak.tey@gmail.com>
2025-08-05 23:53:09 +0330

    Fixed ZeroGrad initialization

--- Codes/attacks/zerograd.py
+++ Codes/attacks/zerograd.py
@@ -4,14 +4,12 @@ import torch.nn.functional as F
 
 def zero_grad(model, x, y, upper_limit, lower_limit, mu, std, epsilon: float = 8/255, alpha: float = 10/255, q_val: float = 0.35, k: float = 1.0, clip: bool = True):
     # Normalize perturbations
-    epsilon = (epsilon / std).view(1, -1, 1, 1)
+    eps = (epsilon / std).view(1, -1, 1, 1)
     alpha = (alpha / std).view(1, -1, 1, 1)
     
-    delta = torch.empty_like(x)
-    if k != 0:
-        for j in range(len(epsilon)):
-            delta[:, j, :, :].uniform_(-k * epsilon[j][0][0].item(), k * epsilon[j][0][0].item())
-        delta = torch.clamp(delta, lower_limit - x, upper_limit - x)
+    # Initialize random step
+    delta = torch.empty_like(x).uniform_(-k, k) * eps
+    delta = torch.clamp(delta, lower_limit - x, upper_limit - x)
     delta.requires_grad = True
 
     output = model(x + delta)
@@ -22,7 +20,7 @@ def zero_grad(model, x, y, upper_limit, lower_limit, mu, std, epsilon: float = 8
 
     delta = delta + alpha * torch.sign(grad)
     if clip:
-        delta = torch.clamp(delta, min=-epsilon, max=epsilon)
+        delta = torch.clamp(delta, min=-eps, max=eps)
     delta = torch.clamp(delta, lower_limit - x, upper_limit - x).detach()
 
     return delta, grad
