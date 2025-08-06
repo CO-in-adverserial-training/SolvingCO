e498ec5b15149d9cead3402040a8f9516e9ba968
Ramtin Moslemi <76493699+RamtinMoslemi@users.noreply.github.com>
2025-08-06 22:33:40 +0330

    Support different channel sizes

--- Codes/architectures/get_model.py
+++ Codes/architectures/get_model.py
@@ -5,7 +5,7 @@ from .senet import SENet18
 from .vit_small import VitSmallPatch16_224
 from .vit_base import VitBasePatch16_224
 
-def get_model(model_name: str, num_classes: int=10, img_size: int=32):
+def get_model(model_name: str, num_classes: int = 10, img_size: int = 32, in_channels: int = 3):
     """
     Get the model for the given model name.
     
@@ -17,31 +17,31 @@ def get_model(model_name: str, num_classes: int=10, img_size: int=32):
     
     match model_name:
         case "ResNet18":
-            return ResNet18(num_classes=num_classes)
+            return ResNet18(num_classes=num_classes, in_channels=in_channels)
         case "ResNet34":
-            return ResNet34(num_classes=num_classes)
+            return ResNet34(num_classes=num_classes, in_channels=in_channels)
         case "ResNet50":
-            return ResNet50(num_classes=num_classes)
+            return ResNet50(num_classes=num_classes, in_channels=in_channels)
         case "ResNet101":
-            return ResNet101(num_classes=num_classes)
+            return ResNet101(num_classes=num_classes, in_channels=in_channels)
         case "ResNet152":
-            return ResNet152(num_classes=num_classes)
+            return ResNet152(num_classes=num_classes, in_channels=in_channels)
         case "PreActResNet18":
-            return PreActResNet18(num_classes=num_classes)
+            return PreActResNet18(num_classes=num_classes, in_channels=in_channels)
         case "PreActResNet34":
-            return PreActResNet34(num_classes=num_classes)
+            return PreActResNet34(num_classes=num_classes, in_channels=in_channels)
         case "PreActResNet50":
-            return PreActResNet50(num_classes=num_classes)
+            return PreActResNet50(num_classes=num_classes, in_channels=in_channels)
         case "PreActResNet101":
-            return PreActResNet101(num_classes=num_classes)
+            return PreActResNet101(num_classes=num_classes, in_channels=in_channels)
         case "PreActResNet152":
-            return PreActResNet152(num_classes=num_classes)
+            return PreActResNet152(num_classes=num_classes, in_channels=in_channels)
         case "WideResNet28":
-            return WideResNet28(num_classes=num_classes)
+            return WideResNet28(num_classes=num_classes, in_channels=in_channels)
         case "WideResNet34":
-            return WideResNet34(num_classes=num_classes)
+            return WideResNet34(num_classes=num_classes, in_channels=in_channels)
         case "SENet18":
-            return SENet18(num_classes=num_classes)
+            return SENet18(num_classes=num_classes, in_channels=in_channels)
         case "VitSmall":
             return VitSmallPatch16_224(num_classes=num_classes, img_size=img_size)
         case "VitBase":
