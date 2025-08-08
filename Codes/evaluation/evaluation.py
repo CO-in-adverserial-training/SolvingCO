Fix accuracy calculation

Codes/evaluation/evaluation.py
@@ -8,7 +8,7 @@ from attacks.get_attack import get_attack
 from attacks.fgsm import fgsm
 from attacks.pgd import pgd
 from attacks.attack_params import get_attack_params
-from training.utils import MetricTracker, calculate_batch_accuracy
+from training.utils import MetricTracker, calculate_batch_corrects
 
 def evaluate(args, device):
     """
@@ -74,8 +74,8 @@ def evaluate(args, device):
                 trackers["reg"].update(batch_test_reg=reg)
             
             # Calculate attack accuracy
-            batch_accuracy = calculate_batch_accuracy(preds, labels)
-            trackers["attack"]["batch"].update(loss=loss.item(), accuracy=batch_accuracy.item())
+            batch_corrects = calculate_batch_corrects(preds, labels)
+            trackers["attack"]["batch"].update(loss=loss.item(), accuracy=batch_corrects.item())
             # Calculate Benign accuracy
             eval_and_track(model, images, labels, torch.zeros_like(images), trackers["benign"]["batch"])
             # Calculate FGSM accuracy
@@ -88,13 +88,13 @@ def evaluate(args, device):
 
         
         attack_epoch_loss = trackers["attack"]["batch"].average("loss")
-        attack_epoch_accuracy = trackers["attack"]["batch"].average("accuracy")
+        attack_epoch_accuracy = trackers["attack"]["batch"].sum("accuracy") / num_test_samples
         benign_epoch_loss = trackers["benign"]["batch"].average("loss")
-        benign_epoch_accuracy = trackers["benign"]["batch"].average("accuracy")
+        benign_epoch_accuracy = trackers["benign"]["batch"].sum("accuracy") / num_test_samples
         fgsm_epoch_loss = trackers["fgsm"]["batch"].average("loss")
-        fgsm_epoch_accuracy = trackers["fgsm"]["batch"].average("accuracy")
+        fgsm_epoch_accuracy = trackers["fgsm"]["batch"].sum("accuracy") / num_test_samples
         pgd_epoch_loss = trackers["pgd"]["batch"].average("loss")
-        pgd_epoch_accuracy = trackers["pgd"]["batch"].average("accuracy")
+        pgd_epoch_accuracy = trackers["pgd"]["batch"].sum("accuracy") / num_test_samples
         
         finish_time = time.time()
         epoch_time = finish_time - start_time
@@ -130,5 +130,5 @@ def eval_and_track(model, images, labels, delta, tracker):
     with torch.no_grad():
         preds = model(images + delta)
         loss = F.cross_entropy(preds, labels)
-        acc = calculate_batch_accuracy(preds, labels)
-        tracker.update(loss=loss.item(), accuracy=acc.item())
+        num_corrects = calculate_batch_corrects(preds, labels)
+        tracker.update(loss=loss.item(), accuracy=num_corrects.item())

Codes/training/utils.py
@@ -12,6 +12,9 @@ class MetricTracker:
         for key, value in kwargs.items():
             self.data[key].append(value)
 
+    def sum(self, key):
+        return sum(self.data[key]) if self.data[key] else 0.0
+
     def average(self, key):
         return sum(self.data[key]) / len(self.data[key]) if self.data[key] else 0.0
 
@@ -77,9 +80,9 @@ def get_input_dimensions(dataloader, index_dataset):
 
     return images.shape
 
-def calculate_batch_accuracy(logits, labels):
+def calculate_batch_corrects(logits, labels):
     """
-    Calculate the accuracy of the model on a batch of data.
+    Calculate the number of correctly classified samples by the model on a batch of data.
     
     Args:
         logits (torch.Tensor): Logits of the model.
@@ -87,4 +90,4 @@ def calculate_batch_accuracy(logits, labels):
     """
     indices = torch.argmax(logits, 1)
     correct_count = (indices == labels).sum()
-    return correct_count / labels.size(0)
+    return correct_count