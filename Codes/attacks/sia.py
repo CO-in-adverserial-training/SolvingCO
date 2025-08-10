fb46f22da3a785cfb8555030bb1f6f149acaf35d
Mazdak Teymourian <mazdak.tey@gmail.com>
2025-08-10 22:38:39 +0330

    Adjusted SIA params

--- Codes/attacks/sia.py
+++ Codes/attacks/sia.py
@@ -63,6 +63,6 @@ def sia_max_alpha_function(func, alignment, max_alpha, a=0.1, b=5, moving_avg_al
             if linearity_coef >= 1:
                 coef = 1
             else:
-                coef = min(1, 0.5 / (1 - linearity_coef))
+                coef = min(1, 0.1 / (1 - linearity_coef))
     alpha = coef * max_alpha
     return alpha
--- Codes/training/train.py
+++ Codes/training/train.py
@@ -52,7 +52,7 @@ def train(args, device):
     
     if args.attack == "SIA":
         attack_params["linearity_coef"] = 1
-        theta = 0.005
+        theta = 0.01
         alignment = None
 
     # Save initial checkpoint
