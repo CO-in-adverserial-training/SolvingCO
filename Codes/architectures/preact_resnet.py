51e43fe69752be3f67900d5b66d7daa6600833ce
Ramtin Moslemi <76493699+RamtinMoslemi@users.noreply.github.com>
2025-08-26 19:58:18 +0330

    Support different input dimensions

--- Codes/architectures/preact_resnet.py
+++ Codes/architectures/preact_resnet.py
@@ -88,7 +88,7 @@ class PreActResNet(nn.Module):
         out = self.layer3(out)
         out = self.layer4(out)
         out = F.relu(self.bn(out))
-        out = F.avg_pool2d(out, 4)
+        out = F.avg_pool2d(out, out.shape[-1])
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
