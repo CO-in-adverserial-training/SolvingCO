84ccce25f7c5b516def84997249b16049a687a9c
Ramtin Moslemi <76493699+RamtinMoslemi@users.noreply.github.com>
2025-10-01 22:53:33 +0330

    Update evaluation.py

--- Codes/evaluation/evaluation.py
+++ Codes/evaluation/evaluation.py
@@ -49,7 +49,9 @@ def evaluate(args, device):
             images, labels = data[0].to(device), data[1].to(device)
 
             match args.attack:
-                case args.attack if args.attack in  ["FGSM", "FGSM-RS", "NFGSM", "ZeroGrad", "MultiGrad", "PGD", "PGD2"]:
+                case "Benign":
+                    delta = torch.zeros_like(images)
+                case args.attack if args.attack in ["FGSM", "FGSM-RS", "NFGSM", "ZeroGrad", "MultiGrad", "PGD", "PGD2"]:
                     delta, _ = attack(model, images, labels, upper_limit, lower_limit, mu, std, **attack_params)
                 case args.attack if args.attack in ["TRADES", "GradAlign", "ELLE"]:
                     delta, reg, _ = attack(model, images, labels, upper_limit, lower_limit, mu, std, **attack_params)
