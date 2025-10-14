ab505d69dec666dccb20d6b6470d2f96de435faf
Mazdak Teymourian <mazdak.tey@gmail.com>
2025-10-14 16:42:56 +0330

    Fix SORA bug

--- Codes/training/linearity_coef.py
+++ Codes/training/linearity_coef.py
@@ -16,8 +16,10 @@ def calc_linearity_coef(input_grad, backprop_grad, method: str):
             linearity_coef = F.cosine_similarity(backprop_grad.view(backprop_grad.shape[0], -1), input_grad.view(input_grad.shape[0], -1)) / torch.pow(norm2_input_grad, 2)
             return linearity_coef.mean().item()
         case "Second Order Theory Sign":
-            norm1_input_grad = torch.linalg.norm(input_grad.view(input_grad.shape[0], -1), dim=1, ord=1)
-            linearity_coef = F.cosine_similarity(backprop_grad.view(backprop_grad.shape[0], -1), torch.sign(input_grad).view(input_grad.shape[0], -1)) / norm1_input_grad
-            return linearity_coef.mean().item()
+            p = torch.sign(input_grad)
+            numerator = (p.view(p.size(0), -1) * backprop_grad.view(backprop_grad.size(0), -1)).sum(dim=1)
+            denominator = input_grad.view(input_grad.size(0), -1).abs().sum(dim=1).clamp_min(1e-12)
+            linearity_coef = (numerator / denominator).mean().item()
+            return linearity_coef
         case _:
             return None
