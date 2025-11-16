3bd1aa761e9f4afcab4244a7df44174b505ca943
Mazdak Teymourian <mazdak.tey@gmail.com>
2025-11-16 23:58:08 +0330

    Fix SORA L2 norm optimal step size

--- Codes/training/linearity_coef.py
+++ Codes/training/linearity_coef.py
@@ -13,7 +13,7 @@ def calc_linearity_coef(input_grad, backprop_grad, method: str):
     match method:
         case "Second Order Theory":
             norm2_input_grad = torch.linalg.norm(input_grad.view(input_grad.shape[0], -1), dim=1, ord=2)
-            linearity_coef = F.cosine_similarity(backprop_grad.view(backprop_grad.shape[0], -1), input_grad.view(input_grad.shape[0], -1)) / torch.pow(norm2_input_grad, 2)
+            linearity_coef = (input_grad.view(input_grad.size(0), -1) * backprop_grad.view(backprop_grad.size(0), -1)).sum(dim=1) / torch.pow(norm2_input_grad, 2)
             return linearity_coef.mean().item()
         case "Second Order Theory Sign":
             p = torch.sign(input_grad)
