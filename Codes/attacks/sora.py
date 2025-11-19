7b34bd3b6401abe9fd6e86ecc2fa3b96022e3463
Mazdak Teymourian <mazdak.tey@gmail.com>
2025-11-19 16:45:05 +0330

    Implemented L2 version for SORA

--- Codes/attacks/sora.py
+++ Codes/attacks/sora.py
@@ -4,14 +4,25 @@ import numpy as np
 
 def sora(model, x, y, upper_limit, lower_limit, mu, std, epsilon: float= 8/255, max_alpha: float=16 / 255, method: str="Second Order Theory Sign", alignment: float=1, prev_batch_alpha: float=None, linearity_coef: float=None):
     # Normalize perturbations
-    eps = (epsilon / std).view(1, -1, 1, 1)
     alpha_scalar = sora_max_alpha_function(method, alignment, max_alpha, a=0.1, b=5, prev_batch_alpha=prev_batch_alpha, linearity_coef=linearity_coef)
-    alpha = (alpha_scalar / std).view(1, -1, 1, 1)
+    if method == "Second Order Theory Sign":
+        eps = (epsilon / std).view(1, -1, 1, 1)
+        alpha = (alpha_scalar / std).view(1, -1, 1, 1)
+    elif method == "Second Order Theory":
+        eps = torch.sqrt(torch.sum((epsilon / std) ** 2)).item()
+        alpha = torch.sqrt(torch.sum((alpha_scalar / std) ** 2)).item()
     
     # Initialize random step
-    k = sora_max_range_noise_function("Fix", alignment, a=2, b=1.5)
-    eta = torch.empty_like(x).uniform_(-k, k)
-    eta *= eps
+    k = sora_max_range_noise_function("Fix", alignment, linearity_coef, alpha_scalar / epsilon, a=2, b=1.5)
+    if method == "Second Order Theory Sign":
+        eta = torch.empty_like(x).uniform_(-k, k)
+        eta *= eps
+    elif method == "Second Order Theory":
+        eta = torch.empty_like(x).normal_()
+        d_flat = eta.view(x.size(0), -1)
+        n = d_flat.norm(p=2, dim=1).view(x.size(0), 1, 1, 1)
+        r = torch.zeros_like(n).uniform_(0, 1)
+        eta *= r / n * eps
     eta = torch.clamp(eta, lower_limit - x, upper_limit - x)
     eta.requires_grad = True
 
@@ -22,7 +33,11 @@ def sora(model, x, y, upper_limit, lower_limit, mu, std, epsilon: float= 8/255, 
     
     # Compute perturbation based on sign of gradient
     interpolation_coeff = torch.rand_like(grad).float()
-    delta = eta + alpha * interpolation_coeff * grad.sign()
+    if method == "Second Order Theory Sign":
+        delta = eta + alpha * interpolation_coeff * grad.sign()
+    elif method == "Second Order Theory":
+        grad_normalized = grad / (grad.view(grad.size(0), -1).norm(p=2, dim=1).view(-1, 1, 1, 1) + 1e-10)
+        delta = eta + alpha * interpolation_coeff * grad_normalized
     delta = torch.clamp(delta, lower_limit - x, upper_limit - x)
     delta = delta.detach()
     
