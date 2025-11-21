c54858c250f9c024db53331b08014cc866497d84
Ramtin Moslemi <76493699+RamtinMoslemi@users.noreply.github.com>
2025-11-21 18:05:47 +0330

    Update attack_params.py

--- Codes/attacks/attack_params.py
+++ Codes/attacks/attack_params.py
@@ -97,7 +97,7 @@ def get_attack_params(args):
         "NuAT": {
             "epsilon": args.epsilon,
             "alpha": args.epsilon,
-            "k": args.epsilon / 2,
+            "k": 0.5,
             "steps": 1,
             "nuc_reg": 4
         },
